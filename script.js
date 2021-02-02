30b1f6c0863732fab483070f2b1240ab0fd6764f
matthewstclaire <matthew.crnich@gmail.com>
2021-02-01 23:15:26 -0700

    refactor success. Now has a smooth scrolldown effect.

--- script.js
+++ script.js
@@ -58,10 +58,12 @@ const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
-  window.scrollTo(
-    s1coords.left + window.pageXOffset,
-    s1coords.top + window.pageYOffset
-  );
+
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
 });
 
 /// styles attributes and classes
